8efa6d0b8240d8490a660f319c39d572389ad036
hedgehog-in-fog <m.tumanova97@gmail.com>
2020-07-23 15:46:10 +0300

    знакомство Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean2));
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.equals(cat2));
         System.out.println(bean.getMessage());
     }
 }
